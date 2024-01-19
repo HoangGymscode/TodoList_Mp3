a095f181ba18139069caec04bac9f37de8ebec8b
HoangGymscode <101062283+HoangGymscode@users.noreply.github.com>
2024-01-19 16:50:22 +0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import {useState} from 'react'
 function App() {
+  const[work, setWork]=useState('')
+  console.log(work) 
   return (
-    <div className="App">
- Tao
+    <div className="flex gap-8 h-screen items-center border-blue-900 px-4 py-2 justify-center">
+      <input
+      type="text"
+      className="outline-none border border-blue-900 px-4 py-2 w-[400px]"
+      value={work}
+      // onChange={e => consol.console.log(e.target.value)}
+      // Hàm set giá trị khi có sự thay đổi cho work
+      onChange={e => setWork(e.target.value)}
+      // onChange={e => setWork(e.target.value)}
+      />
+      <button type="button"
+      className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        Add
+      </button>
+
     </div>
+    
   );
 }
 
